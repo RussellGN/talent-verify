b7663695873f7d401b3930032f485d8e4e62f423
Russellg <dudurussell@gmail.com>
2024-06-20 09:03:19 +0200

    setup: create nextJs app

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+   title: "Talent Verify",
+   description: "Talent Verify is an online talent verification service.",
+};
+
+export default function RootLayout({
+   children,
+}: Readonly<{
+   children: React.ReactNode;
+}>) {
+   return (
+      <html lang="en">
+         <body className={inter.className}>{children}</body>
+      </html>
+   );
+}
