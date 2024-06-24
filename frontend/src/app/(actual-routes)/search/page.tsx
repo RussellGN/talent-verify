8a843fd3951842a4d93f8c2a7798d749c664089c
Russellg <dudurussell@gmail.com>
2024-06-24 19:07:02 +0200

    feat: implement search results page, to be extended later

--- frontend/src/app/(actual-routes)/search/page.tsx
+++ frontend/src/app/(actual-routes)/search/page.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import CareerTimestampCard from "@/app/components/CareerTimestampCard";
+import { sampleCareerTimestamps } from "@/app/data/sampleData";
+import { Grid, Typography } from "@mui/material";
 
-export default function SearchPage() {
-   return <div></div>;
+type PropTypes = {
+   searchParams: {
+      query: string | null;
+      date_started: string | null;
+      date_left: string | null;
+   };
+};
+
+export default function SearchPage({ searchParams: { query, date_started, date_left } }: PropTypes) {
+   const resultsFor = query
+      ? query
+      : date_started
+      ? `date started: ${new Date(date_started).toLocaleDateString()}`
+      : date_left
+      ? `date left: ${new Date(date_left).toLocaleDateString()}`
+      : "";
+
+   return (
+      <div className="min-h-[75vh]">
+         <Typography className="text-center p-4">Showing Results for {`'${resultsFor}'`}</Typography>
+         <Grid container>
+            {sampleCareerTimestamps.map((timeStamp) => (
+               <Grid key={timeStamp.employee.id} item xs={4}>
+                  <CareerTimestampCard careerTimestamp={timeStamp} />
+               </Grid>
+            ))}
+         </Grid>
+      </div>
+   );
 }
