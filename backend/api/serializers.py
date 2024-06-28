dfd91a92dded56b2149b388826af70e6778988c8
Russellg <dudurussell@gmail.com>
2024-06-28 17:28:16 +0200

    feat, refactor: create a more compact employee serializer which doesnt return any nested data but maps to nested attributes

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,50 +1,75 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from .models import Employer, EmployerAdmin, Employee, Department, Role, CareerTimestamp
 
-class EmployerAdminSerializer(ModelSerializer):
+class EmployerAdminSerializer(serializers.ModelSerializer):
    class Meta:
       model = EmployerAdmin
       fields = ['id', 'username']
 
-class EmployerAdminRegistrationSerializer(ModelSerializer):
+class EmployerAdminRegistrationSerializer(serializers.ModelSerializer):
    class Meta:
       model = EmployerAdmin
       fields = ['password', 'username']
 
-class EmployerSerializer(ModelSerializer):
+class EmployerSerializer(serializers.ModelSerializer):
    administrator = EmployerAdminSerializer(many=False, read_only=False)
    class Meta:
       model = Employer
       fields = '__all__'
 
-class EmployerRegistrationSerializer(ModelSerializer):
+class EmployerRegistrationSerializer(serializers.ModelSerializer):
    class Meta:
       model = Employer
       fields = '__all__'
 
-class EmployeeSerializer(ModelSerializer):
+class EmployeeSerializer(serializers.ModelSerializer):
    employer = EmployerSerializer(many=False, read_only=True)
    class Meta:
       model = Employee
       fields = '__all__'
 
-class DepartmentSerializer(ModelSerializer):
+class DepartmentSerializer(serializers.ModelSerializer):
    employer = EmployerSerializer(many=False, read_only=True)
    class Meta:
       model = Department
       fields = '__all__'
 
-class RoleSerializer(ModelSerializer):
+class RoleSerializer(serializers.ModelSerializer):
    department = DepartmentSerializer(many=False, read_only=True)
    class Meta:
       model = Role
       fields = '__all__'
 
-class CareerTimestampSerializer(ModelSerializer):
+class CareerTimestampSerializer(serializers.ModelSerializer):
    employee = EmployeeSerializer(many=False, read_only=True)
    role = RoleSerializer(many=False, read_only=True)
    class Meta:
       model = CareerTimestamp
       fields = '__all__'
+
+class CompactEmployeeSerializer(serializers.ModelSerializer):
+   id = serializers.IntegerField(source='employee.id') 
+   national_id = serializers.CharField(source='employee.national_id') 
+   name = serializers.CharField(source='employee.name') 
+   employee_id = serializers.CharField(source='employee.employee_id') 
+   employer = serializers.CharField(source='employee.employer.name') 
+   department = serializers.CharField(source='role.department.name') 
+   role = serializers.CharField(source='role.title') 
+   duties = serializers.CharField(source='role.duties') 
+
+   class Meta:
+      model = CareerTimestamp
+      fields = [
+         'id',
+         'national_id',
+         'name',
+         'employee_id',
+         'employer',
+         'department',
+         'role',
+         'duties',
+         'date_started',
+         'date_left',
+      ]
 
