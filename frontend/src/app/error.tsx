90918258d9209a3e147ff74b2639b20c4c01c040
Russellg <dudurussell@gmail.com>
2024-06-30 13:07:55 +0200

    refactor: change styling of error page

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { Warning } from "@mui/icons-material";
+import { WarningOutlined } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 
 export default function error({ error: { message } }: { error: { message: string } }) {
    return (
       <div className="min-h-[65vh] flex items-center justify-center">
          <Typography className="text-center p-4">
-            <Warning fontSize="small" sx={{ mr: 0.5, mt: -0.3 }} />
+            <WarningOutlined color="error" fontSize="small" sx={{ mr: 0.5, mt: -0.3 }} />
             Error: {message}
          </Typography>
       </div>
