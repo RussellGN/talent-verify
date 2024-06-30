24119544fac7781f9d9a0e81ff50c75223a14cc4
Russellg <dudurussell@gmail.com>
2024-06-30 13:06:05 +0200

    feat: configure react query to only retry queries once

--- frontend/src/app/providers/ReactQueryProvider.tsx
+++ frontend/src/app/providers/ReactQueryProvider.tsx
@@ -3,7 +3,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+   defaultOptions: {
+      queries: {
+         retry: 1,
+      },
+   },
+});
 
 export default function ReactQueryProvider({ children }: PropsWithChildren) {
    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
