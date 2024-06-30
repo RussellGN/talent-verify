dd1133e96593da7c1b39a299a35d2f4b321a8765
Russellg <dudurussell@gmail.com>
2024-06-30 17:07:41 +0200

    fix, feat: fix axios response error interceptor non-stringified error messages. Create cookie util functions

--- frontend/src/app/lib/index.ts
+++ frontend/src/app/lib/index.ts
@@ -215,7 +215,47 @@ axiosClient.interceptors.response.use(
    (res) => res,
    (error) => {
       if (isAxiosError(error) && error.response?.data) {
-         return Promise.reject(new Error(String(error.response.data)));
+         return Promise.reject(new Error(JSON.stringify(error.response.data)));
       } else return Promise.reject(error);
    }
 );
+
+export function setCookie(cname: string, cvalue: string, exdays: number) {
+   try {
+      const d = new Date();
+      d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+      const expires = "expires=" + d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+   } catch (error) {
+      console.log("error getting cookie:", error);
+   }
+}
+
+export function deleteCookie(cname: string) {
+   try {
+      document.cookie = `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+   } catch (error) {
+      console.log("error deleting cookie:", error);
+   }
+}
+
+export function getCookie(cname: string) {
+   try {
+      const name = cname + "=";
+      const decodedCookie = decodeURIComponent(document.cookie);
+      const ca = decodedCookie.split(";");
+      for (let i = 0; i < ca.length; i++) {
+         let c = ca[i];
+         while (c.charAt(0) == " ") {
+            c = c.substring(1);
+         }
+         if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+         }
+      }
+      return null;
+   } catch (error) {
+      console.log("error getting cookie:", error);
+      return null;
+   }
+}
