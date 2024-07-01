28ef040a74f400c7b6f8ed64ec971752a40bebe6
Russellg <dudurussell@gmail.com>
2024-07-01 11:14:52 +0200

    feat: set query data for 'employees' and 'employer' keys on success

--- frontend/src/app/hooks/useLoginEmployer.ts
+++ frontend/src/app/hooks/useLoginEmployer.ts
@@ -1,11 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import API from "../lib/api";
 import { Credentials } from "../types";
 import { setCookie } from "../lib";
 
 export default function useLoginEmployer() {
+   const queryClient = useQueryClient();
+
    return useMutation({
       mutationFn: (credentials: Credentials) => API.loginEmployer(credentials),
-      onSuccess: (data) => setCookie("token", data.token, 1),
+      onSuccess: (data) => {
+         setCookie("token", data.token, 1);
+         queryClient.setQueryData(["employer", { token: data.token }], {
+            employer: data.employer,
+            employees: data.employees,
+         });
+         queryClient.setQueryData(["employees"], data.employees);
+      },
    });
 }
--- frontend/src/app/hooks/useRegisterEmployer.ts
+++ frontend/src/app/hooks/useRegisterEmployer.ts
@@ -1,11 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import API from "../lib/api";
 import { EmployerRegistrationPayload } from "../types";
 import { setCookie } from "../lib";
 
 export default function useRegisterEmployer() {
+   const queryClient = useQueryClient();
+
    return useMutation({
       mutationFn: (data: EmployerRegistrationPayload) => API.registerEmployer(data),
-      onSuccess: (data) => setCookie("token", data.token, 1),
+      onSuccess: (data) => {
+         setCookie("token", data.token, 1);
+         queryClient.setQueryData(["employer", { token: data.token }], { employer: data.employer, employees: [] });
+         queryClient.setQueryData(["employees"], []);
+      },
    });
 }
