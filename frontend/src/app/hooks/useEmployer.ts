33295ea5620f2a91f8b3d878bd6a9823805cc496
Russellg <dudurussell@gmail.com>
2024-07-01 20:45:54 +0200

    fix: remove redirect if token not found in useEmployer hook

--- frontend/src/app/hooks/useEmployer.ts
+++ frontend/src/app/hooks/useEmployer.ts
@@ -1,13 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import API from "../lib/api";
-import { getCookie } from "../lib/index";
-import { redirect } from "next/navigation";
+import { getCookie } from "../lib";
 
 export default function useEmployer() {
    const token = getCookie("token");
-   if (!token) redirect("/login");
    return useQuery({
       queryKey: ["employer", { token }],
-      queryFn: () => API.getEmployer(token),
+      queryFn: () => API.getEmployer(token || ""),
    });
 }
