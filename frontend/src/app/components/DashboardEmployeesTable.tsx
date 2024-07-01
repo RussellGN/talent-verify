e8a07034841dcd581a8d532c966c59ad4e88fa35
Russellg <dudurussell@gmail.com>
2024-07-01 17:39:46 +0200

    refactor: extract employee table logic into a hook. Change positioning of employee action guidelines

--- frontend/src/app/components/DashboardEmployeesTable.tsx
+++ frontend/src/app/components/DashboardEmployeesTable.tsx
@@ -1,100 +1,60 @@
 "use client";
 
-import * as React from "react";
-import { DataGrid, GridRowModel, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import { DataGrid } from "@mui/x-data-grid";
 import Snackbar from "@mui/material/Snackbar";
-import Alert, { AlertProps } from "@mui/material/Alert";
-import { sampleCareerTimestamps } from "../data/sampleData";
-import { UnormalizedCurrentEmployeeInterface } from "../interfaces";
+import Alert from "@mui/material/Alert";
+import useDashboardEmployeesTable from "../hooks/useDashboardEmployeesTable";
+import { Typography } from "@mui/material";
+import { InfoOutlined } from "@mui/icons-material";
 
 export default function DashboardEmployeesTable() {
-   const [loading, setLoading] = React.useState(false);
+   const { rows, columns, processRowUpdate, handleProcessRowUpdateError, loading, snackbar, handleCloseSnackbar } =
+      useDashboardEmployeesTable();
 
-   function mutateRow(employee: Partial<UnormalizedCurrentEmployeeInterface>) {
-      return new Promise<Partial<UnormalizedCurrentEmployeeInterface>>((resolve, reject) => {
-         setTimeout(() => {
-            if (employee.name?.trim() === "") {
-               reject(new Error("Error while saving employee: name cannot be empty."));
-            } else {
-               resolve({ ...employee, name: employee.name?.toUpperCase() });
-            }
-         }, 2000);
-      });
+   if (rows.length === 0) {
+      return (
+         <div className="text-center bg-slate-50 p-5">
+            <Typography variant="h6">No employees</Typography>
+            <Typography>table of employees will appear after you&apos;ve added or uploaded some</Typography>
+         </div>
+      );
    }
 
-   const [snackbar, setSnackbar] = React.useState<Pick<AlertProps, "children" | "severity"> | null>(null);
-
-   const handleCloseSnackbar = () => setSnackbar(null);
-
-   const processRowUpdate = async (newRow: GridRowModel) => {
-      setLoading(true);
-      // Make the HTTP request to save in the backend
-      const response = await mutateRow(newRow);
-      setSnackbar({ children: "Employee successfully saved", severity: "success" });
-      setLoading(false);
-      return response;
-   };
-
-   const handleProcessRowUpdateError = (error: Error) => {
-      setSnackbar({ children: error.message, severity: "error" });
-   };
-
    return (
-      <div>
-         <DataGrid
-            rows={rows}
-            columns={columns}
-            processRowUpdate={processRowUpdate}
-            onProcessRowUpdateError={handleProcessRowUpdateError}
-            autosizeOnMount
-            disableRowSelectionOnClick
-            // checkboxSelection
-            disableColumnResize
-            disableColumnSelector
-            scrollbarSize={5}
-            // density="compact"
-            loading={loading}
-            showCellVerticalBorder
-            // hideFooter
-         />
-         {!!snackbar && (
-            <Snackbar
-               open
-               anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-               onClose={handleCloseSnackbar}
-               autoHideDuration={6000}
-            >
-               <Alert {...snackbar} onClose={handleCloseSnackbar} />
-            </Snackbar>
-         )}
-      </div>
+      <>
+         <div className="p-5 ">
+            <Typography variant="subtitle2">
+               <InfoOutlined color="warning" fontSize="inherit" sx={{ mt: -0.3, mr: 0.5 }} />
+               To update an employee&apos;s details, click in any data cell and enter a new value. You will be prompted for
+               confirmation before saving.
+            </Typography>
+         </div>
+
+         <div>
+            <DataGrid
+               rows={rows}
+               columns={columns}
+               processRowUpdate={processRowUpdate}
+               onProcessRowUpdateError={handleProcessRowUpdateError}
+               autosizeOnMount
+               disableRowSelectionOnClick
+               disableColumnResize
+               disableColumnSelector
+               scrollbarSize={5}
+               loading={loading}
+               showCellVerticalBorder
+            />
+            {!!snackbar && (
+               <Snackbar
+                  open
+                  anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                  onClose={handleCloseSnackbar}
+                  autoHideDuration={6000}
+               >
+                  <Alert {...snackbar} onClose={handleCloseSnackbar} />
+               </Snackbar>
+            )}
+         </div>
+      </>
    );
 }
-
-const columns: GridColDef[] = [
-   { field: "id", headerName: "ID", type: "string" },
-   { field: "national_id", headerName: "National ID", type: "string", editable: true },
-   { field: "name", headerName: "Name", type: "string", editable: true },
-   // { field: "employer", headerName: "Employer", type: "string" },
-   { field: "employee_id", headerName: "Employee ID", type: "string", editable: true },
-   { field: "department", headerName: "Department", type: "string", editable: true },
-   { field: "role", headerName: "Role", type: "string", editable: true },
-   { field: "duties", headerName: "Duties", type: "string", editable: true },
-   { field: "date_started", headerName: "Date Started", type: "date", editable: true },
-   { field: "date_left", headerName: "Date Left", type: "date", editable: true },
-];
-
-const relevantTimestamps: UnormalizedCurrentEmployeeInterface[] = sampleCareerTimestamps.map((stamp) => ({
-   id: stamp.employee.id,
-   national_id: stamp.employee.national_id,
-   name: stamp.employee.name,
-   // employer: stamp.employee.employer?.name,
-   employee_id: stamp.employee.employee_id,
-   department: stamp.role.department.name,
-   role: stamp.role.title,
-   duties: stamp.role.duties,
-   date_started: stamp.date_started ? new Date(stamp.date_started) : undefined,
-   date_left: stamp.date_left ? new Date(stamp.date_left) : undefined,
-}));
-
-const rows: GridRowsProp = relevantTimestamps;
---
+++ frontend/src/app/hooks/useDashboardEmployeesTable.ts
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { UnormalizedCurrentEmployeeInterface } from "../interfaces";
+import { AlertProps } from "@mui/material";
+import { GridColDef, GridRowModel, GridRowsProp } from "@mui/x-data-grid";
+import useEmployees from "./useEmployees";
+
+export default function useDashboardEmployeesTable() {
+   const { isPending, isError, error, data } = useEmployees();
+   const [loading, setLoading] = useState(false);
+
+   console.log({ isPending, isError, error, data });
+
+   function mutateRow(employee: Partial<UnormalizedCurrentEmployeeInterface>) {
+      return new Promise<Partial<UnormalizedCurrentEmployeeInterface>>((resolve, reject) => {
+         setTimeout(() => {
+            if (employee.name?.trim() === "") {
+               reject(new Error("Error while saving employee: name cannot be empty."));
+            } else {
+               resolve({ ...employee, name: employee.name?.toUpperCase() });
+            }
+         }, 2000);
+      });
+   }
+
+   async function processRowUpdate(newRow: GridRowModel, oldRow: GridRowModel) {
+      console.log(oldRow);
+      setLoading(true);
+      // Make the HTTP request to save in the backend
+      const response = await mutateRow(newRow);
+      setSnackbar({ children: "Employee successfully saved", severity: "success" });
+      setLoading(false);
+      return response;
+   }
+
+   const handleProcessRowUpdateError = (error: Error) => {
+      setSnackbar({ children: error.message, severity: "error" });
+   };
+
+   const [snackbar, setSnackbar] = useState<Pick<AlertProps, "children" | "severity"> | null>(null);
+
+   const handleCloseSnackbar = () => setSnackbar(null);
+
+   const dataAvailable = !(isPending || isError);
+
+   const rows: GridRowsProp & UnormalizedCurrentEmployeeInterface[] = dataAvailable ? data : [];
+
+   const columns: GridColDef[] = [
+      { field: "id", headerName: "ID", type: "string" },
+      { field: "national_id", headerName: "National ID", type: "string", editable: true },
+      { field: "name", headerName: "Name", type: "string", editable: true },
+      { field: "employee_id", headerName: "Employee ID", type: "string", editable: true },
+      { field: "department", headerName: "Department", type: "string", editable: true },
+      { field: "role", headerName: "Role", type: "string", editable: true },
+      { field: "duties", headerName: "Duties", type: "string", editable: true },
+      { field: "date_started", headerName: "Date Started", type: "date", editable: true },
+      { field: "date_left", headerName: "Date Left", type: "date", editable: true },
+   ];
+
+   return {
+      columns,
+      rows,
+      processRowUpdate, // not handled
+      loading, // not handled
+      handleProcessRowUpdateError, // not handled
+      snackbar, // not handled
+      handleCloseSnackbar, // not handled
+   };
+}
