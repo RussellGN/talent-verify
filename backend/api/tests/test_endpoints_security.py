81f2e57d6b0fefea46225ef87b74acc8c52c7172
Russellg <dudurussell@gmail.com>
2024-07-09 10:17:41 +0200

    refactor, test: remove non-security tests. Rename test files as security tests

--- backend/api/tests/test_endpoints_security.py
+++ backend/api/tests/test_endpoints_security.py
@@ -1,32 +1,8 @@
 from rest_framework import status
-from rest_framework.authtoken.models import Token
-
 from .test_setup import TestSetup
-from ..models import EmployerAdmin, Employer
-from ..serializers import EmployerRegistrationSerializer
 
-class TestViews(TestSetup):
-
-   # ______test GET '/' endpoint______
-
-   def test_can_get_endpoints(self):
-      res = self.client.get(self.get_endpoints_url)
-      self.assertEqual(res.status_code, status.HTTP_200_OK)
-
-   def test_cannot_use_http_methods_other_than_GET_on_get_endpoints_route(self):
-      res0 = self.client.get(self.get_endpoints_url)
-      res1 = self.client.post(self.get_endpoints_url)
-      res2 = self.client.patch(self.get_endpoints_url)
-      res3 = self.client.put(self.get_endpoints_url)
-      res4 = self.client.delete(self.get_endpoints_url)
-
-      self.assertEqual(res0.status_code, status.HTTP_200_OK)
-      self.assertEqual(res1.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-      self.assertEqual(res2.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-      self.assertEqual(res3.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-      self.assertEqual(res4.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
-   # ______test POST 'employer/register/' endpoint______
+class TestEndpointsSecurity(TestSetup):
+   # ______test security for POST 'employer/register/' endpoint______
 
    def test_can_register_with_proper_data(self):
       res = self.client.post(self.register_employer_url, self.employer_and_employer_admin_registration_data_complete , format='json')
@@ -44,14 +20,6 @@ class TestViews(TestSetup):
       self.assertEqual(res2.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
       self.assertEqual(res3.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
       self.assertEqual(res4.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
-   def test_cannot_register_without_providing_any_json_data(self):
-      res = self.client.post(self.register_employer_url)
-      self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
-   def test_cannot_register_with_empty_data(self):
-      res = self.client.post(self.register_employer_url, {}, format='json')
-      self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
    def test_cannot_register_without_providing_employer_admin_details(self):
       res = self.client.post(self.register_employer_url, {'employer': self.employer_registration_data}, format='json')
@@ -71,25 +39,7 @@ class TestViews(TestSetup):
       }, format='json')
       self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-   def test_cannot_register_without_providing_employer_details(self):
-      res = self.client.post(self.register_employer_url, {'employer-admin': self.employer_admin_credentials}, format='json')
-      self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
-   def test_cannot_register_without_providing_employer_name(self):
-      res = self.client.post(self.register_employer_url, {
-         'employer-admin': self.employer_admin_credentials,
-         'employer': self.employer_registration_data_no_name,
-      }, format='json')
-      self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
-   def test_cannot_register_with_non_ISO_date_value_for_registration_date(self):
-      res = self.client.post(self.register_employer_url, {
-         'employer-admin': self.employer_admin_credentials,
-         'employer': self.employer_registration_data_incorrect_date_format,
-      }, format='json')
-      self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
-   # ______test GET 'employer/' endpoint______
+   # ______test security for GET 'employer/' endpoint______
 
    def test_can_get_employer_details_with_valid_auth_token(self):
       registration_res = self.client.post(self.register_employer_url, self.employer_and_employer_admin_registration_data_complete , format='json')
@@ -122,12 +72,16 @@ class TestViews(TestSetup):
       self.assertEqual(res3.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
       self.assertEqual(res4.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-   # ______test POST 'employer/login/' endpoint______
+   # ______test security for POST 'employer/login/' endpoint______
 
    def test_can_login_with_credentials(self):
       self.client.post(self.register_employer_url, self.employer_and_employer_admin_registration_data_complete , format='json')
       res = self.client.post(self.login_employer_url, self.employer_admin_credentials, format='json')
       self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+   def test_cannot_login_with_incorrect_credentials(self):
+      res = self.client.post(self.login_employer_url, self.employer_admin_credentials, format='json')
+      self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
    def test_cannot_use_http_methods_other_than_POST_on_login_employer_route(self):
       self.client.post(self.register_employer_url, self.employer_and_employer_admin_registration_data_complete , format='json')
@@ -164,7 +118,7 @@ class TestViews(TestSetup):
       res = self.client.post(self.login_employer_url, self.employer_admin_credentials_no_password, format='json')
       self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-   # ______test POST 'employer/logout/' endpoint______
+   # ______test security for POST 'employer/logout/' endpoint______
 
    def test_can_logout_with_valid_auth_token(self):
       registration_res = self.client.post(self.register_employer_url, self.employer_and_employer_admin_registration_data_complete , format='json')
@@ -207,9 +161,9 @@ class TestViews(TestSetup):
       self.assertEqual(res3.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
       self.assertEqual(res4.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-   # ______test PATCH 'employer/update/' endpoint______
+   # ______test security for PATCH 'employer/update/' endpoint______
 
-   def test_can_update_employer_with_proper_data(self):
+   def test_can_update_employer_with_proper_data_and_valid_auth_token(self):
       registration_res = self.client.post(self.register_employer_url, self.employer_and_employer_admin_registration_data_complete , format='json')
       token = registration_res.data['token']
       res = self.client.patch(self.patch_employer_url, self.employer_and_employer_admin_patch_data_complete, format='json', headers={'Authorization': f'Token {token}'})
@@ -235,43 +189,11 @@ class TestViews(TestSetup):
       res = self.client.patch(self.patch_employer_url, self.employer_and_employer_admin_patch_data_complete, format='json')
       self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
-   def test_cannot_update_employer_without_providing_any_json_data(self):
-      registration_res = self.client.post(self.register_employer_url, self.employer_and_employer_admin_registration_data_complete , format='json')
-      token = registration_res.data['token']
-      res = self.client.patch(self.patch_employer_url, headers={'Authorization': f'Token {token}'})
-      self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+   def test_cannot_update_employer_with_invalid_auth_token(self):
+      res = self.client.patch(self.patch_employer_url, self.employer_and_employer_admin_patch_data_complete, format='json', headers={'Authorization': f'Token invalid_token'})
+      self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
-   def test_can_update_employer_with_partial_data(self):
-      registration_res = self.client.post(self.register_employer_url, self.employer_and_employer_admin_registration_data_complete , format='json')
-      token = registration_res.data['token']
-
-      res1 = self.client.patch(self.patch_employer_url, {}, format='json', headers={'Authorization': f'Token {token}'})
-      res2 = self.client.patch(self.patch_employer_url, {'employer': self.employer_registration_data_only_name}, format='json', headers={'Authorization': f'Token {token}'})
-      res3 = self.client.patch(self.patch_employer_url, {'employer-admin': self.employer_admin_credentials_no_username}, format='json', headers={'Authorization': f'Token {token}'})
-      res4 = self.client.patch(self.patch_employer_url, {'employer-admin': self.employer_admin_credentials_no_password}, format='json', headers={'Authorization': f'Token {token}'})
-      res4 = self.client.patch(self.patch_employer_url, {
-         'employer-admin': self.employer_admin_credentials_no_password,
-         'employer': self.employer_registration_data_only_name,
-         'departments': ['customer service'],
-      }, format='json', headers={'Authorization': f'Token {token}'})
-
-      self.assertEqual(res1.status_code, status.HTTP_200_OK)
-      self.assertEqual(res2.status_code, status.HTTP_200_OK)
-      self.assertEqual(res3.status_code, status.HTTP_200_OK)
-      self.assertEqual(res4.status_code, status.HTTP_200_OK)
-
-   def test_cannot_update_employer_with_non_ISO_date_value_for_registration_date(self):
-      registration_res = self.client.post(self.register_employer_url, self.employer_and_employer_admin_registration_data_complete , format='json')
-      token = registration_res.data['token']
-
-      res = self.client.patch(self.patch_employer_url, {
-         'employer-admin': self.employer_admin_credentials,
-         'employer': self.employer_registration_data_incorrect_date_format,
-      }, format='json', headers={'Authorization': f'Token {token}'})
-
-      self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
-   # ______test GET 'employees/' endpoint______
+   # ______test security for GET 'employees/' endpoint______
 
    def test_can_get_employees_with_valid_auth_token(self):
       registration_res = self.client.post(self.register_employer_url, self.employer_and_employer_admin_registration_data_complete , format='json')
@@ -288,12 +210,27 @@ class TestViews(TestSetup):
       res = self.client.get(self.handle_employees_url, headers={'Authorization': 'Token invalid_token'})
       self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
-   # ______test POST 'employees/' endpoint______
+   # ______test security for POST 'employees/' endpoint______
 
-   # ______test PATCH 'employees/' endpoint______
+   def test_can_post_employee_with_valid_auth_token_and_valid_data(self):
+      registration_res = self.client.post(self.register_employer_url, self.employer_and_employer_admin_registration_data_complete , format='json')
+      token = registration_res.data['token']
 
-   # ______test DELETE 'employees/(ID)/' endpoint______
+      res = self.client.post(self.handle_employees_url, self.employees_creation_data_single_and_complete, format='json', headers={'Authorization': f'Token {token}'})
+      self.assertEqual(res.status_code, status.HTTP_201_CREATED)
 
-   # ______test GET 'talent?query=(query),is_date=(is_date)/' endpoint______
+   def test_cannot_post_employee_without_auth_token(self):
+      res = self.client.post(self.handle_employees_url, self.employees_creation_data_single_and_complete, format='json')
+      self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
-   # ______test GET 'talent/(ID)/' endpoint______
+   def test_cannot_post_employee_with_invalid_auth_token(self):
+      res = self.client.post(self.handle_employees_url, self.employees_creation_data_single_and_complete, format='json', headers={'Authorization': f'Token invalid-token'})
+      self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+   # ______test security for PATCH 'employees/' endpoint______
+
+   # ______test security for DELETE 'employees/(ID)/' endpoint______
+
+   # ______test security for GET 'talent?query=(query),is_date=(is_date)/' endpoint______
+
+   # ______test security for GET 'talent/(ID)/' endpoint______
